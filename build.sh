65cfab335e0a4855a3a91f0e954095d409f6aa83
lrr68 <luccaaugusto@protonmail.ch>
2021-02-26 08:09:58 -0300

    update

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+uploadarquivos()
+{
+	echo "" > _data/piadas.yml
+	for FOTO in $(ls --sort=extension piadas)
+	do
+		echo "-   nome: $FOTO" >> _data/piadas.yml
+		echo "    link: /images/fotos-skate/$FOTO" >> _data/piadas.yml
+		echo "" >> _data/piadas.yml
+	done
+}
+
+#. compress_img.sh
+#rm images/*.png
+uploadarquivos
+
+bundle update &&
+JEKYLL_ENV=production &&
+bundle exec jekyll build &&
+
+cd _site && rm *.sh && cd ..
--- uploadarquivos.sh
+++ uploadarquivos.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-mv _data/portfolio.yml _data/old-portfolio.yml
-echo "" > _data/portfolio.yml
-for FOTO in $(ls portfolio | grep jpg)
-do
-	echo "-   nome: $FOTO" >> _data/portfolio.yml
-	echo "    link: /portfolio/$FOTO" >> _data/portfolio.yml
-done 
-for FOTO in $(ls piadas | grep jpg)
+#mv _data/portfolio.yml _data/old-portfolio.yml
+#echo "" > _data/portfolio.yml
+#for FOTO in $(ls portfolio | grep jpg)
+#do
+#	echo "-   nome: $FOTO" >> _data/portfolio.yml
+#	echo "    link: /portfolio/$FOTO" >> _data/portfolio.yml
+#done 
+
+echo "" > _data/piadas.yml
+for FOTO in $(ls piadas)
 do
 	echo "-   nome: $FOTO" >> _data/piadas.yml
 	echo "    link: /piadas/$FOTO" >> _data/piadas.yml
