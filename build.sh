01362f19b2518b70adf96a909ae6b786709debce
luccaugusto <lucca@luccaaugusto.xyz>
2023-08-07 15:02:28 -0300

    remove old portfolio stuff

--- build.sh
+++ build.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-uploadarquivos()
-{
-	echo "" > _data/piadas.yml
-	for FOTO in $(ls --sort=extension piadas)
-	do
-		echo "-   nome: $FOTO" >> _data/piadas.yml
-		echo "    link: /piadas/$FOTO" >> _data/piadas.yml
-		echo "" >> _data/piadas.yml
-	done
-}
-
-#. compress_img.sh
-#rm images/*.png
-uploadarquivos
-
 bundle update &&
 JEKYLL_ENV=production &&
 bundle exec jekyll build &&
--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "UPLOADING"
-rsync -avz _site/ root@luccaaugusto.xyz:/var/www/portfolio.luccaaugusto.xyz/html/
-
-echo "UPDATE GIT"
-git add .
-git commit && #-m "$MSG"
-git push
