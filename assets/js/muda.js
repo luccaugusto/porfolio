d7dc220395beffbe00cdac408e2bc909a2306622
Lucca Augusto <lucca@luccaaugusto.xyz>
2023-09-05 19:16:59 +0000

    start changing bio and about page

--- assets/js/muda.js
+++ assets/js/muda.js
@@ -2,11 +2,10 @@ function  toggle_menu() {
 	var menu = document.getElementById("menu-container");
 
   menu.classList.toggle("menu-container-open");
-	if (menu.style.display !== 'block') {
-		/* menu.style.display = 'block';
-    menu.style.opacity = 1; */
-	}else{
-		/* menu.style.display = 'none';
-    menu.style.opacity = 0; */
-	}
+}
+
+function expandir() {
+	var extra_bio = document.getElementById("mais-bio");
+
+  extra_bio.classList.toggle("hidden");
 }
